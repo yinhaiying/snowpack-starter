6590d969b57228325f2462a4606d025ee43282ab
yinhaiying <yinhaiying@58.com>
2020-07-28 21:07:01 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,7 @@ function App({}: AppProps) {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>hello,snowpack</p>
       </header>
     </div>
   );
